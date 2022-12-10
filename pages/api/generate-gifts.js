7d1be8df2e4df5a8f9ef2e74fecfcaebf305704b
Rokas <rookas.rudzenskas@gmail.com>
2022-12-10 09:51:25 +0200

    feat: add OpenAI API
    This commit adds the OpenAI API to the project.
    
    It uses the OpenAI API to generate gift ideas based on the user's input.

--- pages/api/generate-gifts.js
+++ pages/api/generate-gifts.js
@@ -1,1 +1,21 @@
-// suggest 3 Christmas gift ideas between 50$ and 100$ for a 30 years old man that is into golfing, business, traveling.
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+    apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+export default async function (req, res) {
+    const { priceMin, priceMax, gender, age, hobbies } = req.body;
+    const completion = await openai.createCompletion({
+        model: 'text-davinci-003',
+        prompt: generatePrompt(priceMin, priceMax, gender, age, hobbies),
+        temperature: 0.6,
+        max_tokens: 2048,
+    });
+    res.status(200).json({ result: completion.data.choices[0].text });
+}
+function generatePrompt(priceMin, priceMax, gender, age, hobbies) {
+    return `suggest 3 Christmas gift ideas between ${priceMin}$ and ${priceMax}$ for a ${age} years old ${gender} that is into ${hobbies}.`;
+}
